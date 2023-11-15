d5e9e91297099ec4d26bd3481f359c8d95e8168e
IRamNoc <90531307+IRamNoc@users.noreply.github.com>
2023-11-15 12:13:25 +0100

    Changement model et ajout du training

--- model.py
+++ model.py
@@ -1,16 +1,13 @@
 import tensorflow as tf
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense
-from tensorflow.keras.layers import Dropout
+from tensorflow.keras.layers import Dense, Dropout
 
-model = Sequential()
-model.add(Dense(64, input_dim=11, activation='relu'))
-model.add(Dropout(0.5))  # Ajout de la couche de dropout
-model.add(Dense(32, activation='relu'))
-model.add(Dropout(0.5))  # Ajout de la couche de dropout
-model.add(Dense(1, activation='linear'))
-model.compile(optimizer='adam', loss='mean_squared_error', metrics=['mae'])
-
-
-# Affichage de la structure du modèle
-model.summary()
+def create_model():
+    model = Sequential()
+    model.add(Dense(64, input_dim=11, activation='relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(32, activation='relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(1, activation='sigmoid'))  # Utilisation de 'sigmoid' pour garantir des prédictions entre 0 et 1
+    model.compile(optimizer='adam', loss='mean_squared_error', metrics=['mae'])
+    return model
---
+++ training.py
@@ -0,0 +1,58 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from model import create_model
+
+import matplotlib.pyplot as plt
+
+#Charger le CSV
+df = pd.read_csv('Wines.csv')
+
+# Diviser les données en caractéristiques (X) et la variable cible (y)
+X = df.drop(['quality', 'Id'], axis=1)  # Exclure 'quality' et 'Id' des caractéristiques
+y = df['quality'] /10 
+
+# Diviser les données en ensembles d'entraînement et de test
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Afficher les dimensions des ensembles d'entraînement et de test
+print("Dimensions de l'ensemble d'entraînement (X_train, y_train):", X_train.shape, y_train.shape)
+print("Dimensions de l'ensemble de test (X_test, y_test):", X_test.shape, y_test.shape)
+
+
+
+
+# Créer le modèle en utilisant la fonction définie dans model.py
+model = create_model()
+
+# Entraîner le modèle avec les données d'entraînement
+history = model.fit(X_train, y_train, epochs=50, batch_size=32, validation_data=(X_test, y_test))
+
+
+
+
+
+# Sélectionner une ligne particulière (par exemple, la première ligne)
+sample_row = df.iloc[[7]]
+
+# Exclure 'Id' de la caractéristique
+X_sample = sample_row.drop(['quality', 'Id'], axis=1)
+
+# Utiliser le modèle pour faire une prédiction
+prediction = model.predict(X_sample)
+
+# Afficher la prédiction
+print("Prediction:", prediction)
+
+
+
+
+
+# Afficher la courbe de la loss pendant l'entraînement
+plt.plot(history.history['loss'], label='Training Loss')
+plt.plot(history.history['val_loss'], label='Validation Loss')
+plt.xlabel('Epochs')
+plt.ylabel('Mean Squared Error Loss')
+plt.legend()
+plt.show()
+
+
